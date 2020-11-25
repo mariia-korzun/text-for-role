becf276e8926d90eafcefaf1e1c4ceceb46ca0f6
Mariia Korzun <3151020m@gmail.com>
2020-11-25 02:06:38 +0200

    Text for role task is done.

--- src/text-for-role.js
+++ src/text-for-role.js
@@ -1,6 +1,20 @@
 const textForRole = (roles, textLines) => {
-    // Your implementation here
+    let result = ''
+    let phrases = textLines.split('\n')
+    let numberOfNewLineSymbols = 2
+
+    roles.forEach(role => {
+        result += `\n\n${role}:`
+        phrases.forEach((phrase, phraseIndex) => {
+            let regExp = new RegExp(`^${role}:`)
+            if (regExp.test(phrase)) {
+                phrase = phrase.replace(`${role}:`, '')
+                result += `\n${phraseIndex + 1})${phrase}`
+            }
+        })
+
+    })
+    return result.slice(numberOfNewLineSymbols)
 }
 
 module.exports = textForRole;
-
